1137e6113de1b4e09d37283b996d7fa7b124df05
Zoranner <zoran_young@hotmail.com>
2023-06-10 17:39:27 +0800

    统一管理需全局有效的变量

--- src/store/globalStore.ts
+++ src/store/globalStore.ts
@@ -1,4 +1,10 @@
 export const globalData = reactive({
-	/** 标签栏选中的index */
-	tabBarSelected: 1
+	/** 计时器状态Index */
+	timerState: false,
+	/** 音效状态 */
+	audioState: true,
+	/** 震动状态 */
+	vibrateState: true,
+	/** 标签栏选中的Index */
+	tabBarSelected: 1,
 })
