58491f25233f209aabf0a31bae7047698b4873f4
Zoranner <zoran_young@hotmail.com>
2023-06-23 03:35:19 +0800

    删除不需要的脚本文件

--- src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @returns 当天00:00点的时间戳
- */
-export const thatDayStart = () => new Date().setHours(0, 0, 0, 0).valueOf()
-
-/**
- * @returns 获取当天23:59:59时间戳
- */
-export const thatDayEnd = () => new Date().setHours(23, 59, 59, 999).valueOf()
--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './changeTextToCN'
-export * from './date'
-export * from './getNowWeek'
-export * from './setReactive'
