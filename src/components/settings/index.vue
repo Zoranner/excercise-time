1d0efd7eeaf65e7364a5f7b761e450e4d2d8774c
Zoranner <zoran_young@hotmail.com>
2023-06-23 03:40:56 +0800

    新增设置项组件并更新设置列表

--- src/components/settings/index.vue
+++ src/components/settings/index.vue
@@ -1,11 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { globalStore } from '@/store/globalStore'
+import SettingItem from './setting-item.vue';
+import CheckBox from '@/components/base/check-box/index.vue'
+import AboutCard from '@/components/about/index.vue'
+
+const vibrateStateChecked = (checked: boolean) => {
+	globalStore.vibrateState.value = checked
+}
+</script>
 
 <template>
 	<view class="settingsPage">
 		<scroll-view class="settingsScrollView" :scroll-y="true">
-			<view v-for="index in 3" :key="index">
-				<view />
-			</view>
+			<SettingItem caption="震动辅助">
+				<CheckBox class="settingCheckBox" :checked="globalStore.vibrateState" @update:checked="vibrateStateChecked" />
+			</SettingItem>
+			<!-- <SettingItem caption="当前版本" :value="globalConst.programVersion" />
+			<SettingItem caption="开发人员" :value="globalConst.programAuthor" />
+			<SettingItem caption="美术设计" :value="globalConst.programDesign" />
+			<SettingItem caption="专业顾问" :value="globalConst.programCounselor" /> -->
+			<AboutCard />
 			<view class="settingsPlaceholder"></view>
 		</scroll-view>
 	</view>
@@ -15,9 +29,11 @@
 .settingsPage {
 	height: 100%;
 	background: var(--color-dark-gray);
+
 	.settingsScrollView {
 		width: 100%;
-		height: 100%;
+		height: auto;
+
 		// background: var(--color-white);
 		.settingsPlaceholder {
 			width: 100%;
---
+++ src/components/settings/setting-item.vue
@@ -0,0 +1,81 @@
+<script lang="ts" setup>
+const props = defineProps({
+	caption: {
+		type: String,
+		default: '设置项'
+	},
+	value: {
+		type: String,
+		default: '值'
+	},
+})
+
+const slotDefault = !!useSlots().default
+</script>
+
+<template>
+	<view class="settingItemPanel w-100% flex items-center justify-center">
+		<!-- <radio class="settingRadio" :checked="props.checked"></radio> -->
+		<view class="settingItemCaption flex-1">{{ props.caption }}</view>
+		<view class="settingItemValueArea">
+			<view class="settingItemCustom" v-if="slotDefault">
+				<slot></slot>
+			</view>
+			<text class="settingItemValue" v-if="!slotDefault">{{ props.value }}</text>
+			<!-- <CheckBox class="settingCheckBox" :checked=true /> -->
+		</view>
+	</view>
+</template>
+
+<style lang="scss">
+.settingItemPanel {
+	width: calc(100% - 40px);
+	height: 150px;
+	margin: 30px 20px;
+	padding: 10px 0px;
+	border-radius: 30px;
+	color: var(--color-white);
+	background: var(--color-trans-black);
+
+	.settingItemCaption {
+		//width: calc(100% - 45px);
+		font-size: 1.35em;
+		margin-left: 50px;
+		filter: var(--shadow-drop-white);
+	}
+
+	.settingItemValueArea {
+		position: relative;
+		width: 300px;
+		height: 100%;
+		// margin-right: 50px;
+		filter: var(--shadow-drop-white);
+
+		.settingItemCustom {
+			position: absolute;
+			right: 50px;
+			top: 50%;
+			transform: translate(0%, -50%);
+		}
+
+		.settingItemValue {
+			position: absolute;
+			width: 100%;
+			height: auto;
+			text-align: right;
+			right: 50px;
+			top: 50%;
+			font-size: 1.1em;
+			transform: translate(0%, -50%);
+		}
+	}
+
+	.settingItemValueArea:active {
+		filter: var(--shadow-drop-heavy-white);
+	}
+}
+
+// .settingItemPanel:active {
+// 	background: var(--activeBackground);
+// }
+</style>
