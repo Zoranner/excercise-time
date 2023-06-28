af00a1b87b8b2f740db1f9a5f19eb2880ba63497
Zoranner <zoran_young@hotmail.com>
2023-06-28 10:43:27 +0800

    优化关于组件的显示内容和效果

--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -7,23 +7,23 @@
 			<image class="aboutLogoImage" src="@/assets/images/logo.png"></image>
 		</view>
 		<view class="aboutTitle">间歇锻炼计时器</view>
-		<view class="aboutInformation">当前版本：{{ globalConst.programVersion }}</view>
+		<!-- <view class="aboutInformation">当前版本：{{ globalConst.programVersion }}</view>
 		<view class="aboutInformation">程序设计：{{ globalConst.programAuthor }}</view>
 		<view class="aboutInformation">美术设计：{{ globalConst.programDesign }}</view>
-		<view class="aboutInformation">专业顾问：{{ globalConst.programCounselor }}</view>
+		<view class="aboutInformation">专业顾问：{{ globalConst.programCounselor }}</view> -->
 	</view>
 </template>
 
 <style lang="scss">
 .aboutComponent {
-	margin-top: 120px;
+	margin: 120px 0;
 	width: 100%;
 	height: auto;
 	text-align: center;
 	line-height: 1.8em;
-	opacity: 0.6;
+	opacity: 0.4;
 	color: var(--color-light-gray);
-	filter: var(--shadow-drop-white);
+	// filter: var(--shadow-drop-white);
 
 	.aboutLogo {
 		width: 120px;
