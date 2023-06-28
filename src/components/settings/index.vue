8be00cfce5a73eeed6cb3a55cb5fe721a23e1b48
Zoranner <zoran_young@hotmail.com>
2023-06-28 10:49:12 +0800

    优化设置页面的显示内容和效果

--- src/components/settings/index.vue
+++ src/components/settings/index.vue
@@ -1,26 +1,57 @@
 <script lang="ts" setup>
+import GroupItem from './group-item.vue';
 import SettingItem from './setting-item.vue';
 import CheckBox from '@/components/base/check-box/index.vue'
 import AboutCard from '@/components/about/index.vue'
+import SurpriseCanvas from '@/components/surprise/index.vue'
+
+let aboutTouchTime = 0;
+const surpriseActive = ref(false)
+const surprisePanel = ref();
 
 const vibrateStateChecked = (checked: boolean) => {
 	globalStore.vibrateState.value = checked
+}
+
+const aboutCardClicked = () => {
+	if (aboutTouchTime == 0) {
+		aboutTouchTime = new Date().getTime();
+	} else {
+		if (((new Date().getTime()) - aboutTouchTime) < 800) {
+			surpriseActive.value = true
+			surprisePanel.value.surpriseClicked()
+			aboutTouchTime = 0;
+		} else {
+			aboutTouchTime = new Date().getTime();
+		}
+	}
+}
+
+const surpriseStoped = () => {
+	surpriseActive.value = false
 }
 </script>
 
 <template>
 	<view class="settingsPage">
 		<scroll-view class="settingsScrollView" :scroll-y="true">
-			<SettingItem caption="震动辅助">
-				<CheckBox class="settingCheckBox" :checked="globalStore.vibrateState" @update:checked="vibrateStateChecked" />
+			<GroupItem caption="辅助"/>
+			<SettingItem caption="语音辅助">
+				<CheckBox class="settingCheckBox" :checked="globalStore.audioState" @update:checked="vibrateStateChecked" />
 			</SettingItem>
-			<!-- <SettingItem caption="当前版本" :value="globalConst.programVersion" />
+			<SettingItem caption="震动辅助">
+				<CheckBox class="settingCheckBox" :checked="globalStore.vibrateState"
+					@update:checked="vibrateStateChecked" />
+			</SettingItem>
+			<GroupItem caption="关于"/>
+			<SettingItem caption="当前版本" :value="globalConst.programVersion" />
 			<SettingItem caption="开发人员" :value="globalConst.programAuthor" />
 			<SettingItem caption="美术设计" :value="globalConst.programDesign" />
-			<SettingItem caption="专业顾问" :value="globalConst.programCounselor" /> -->
-			<AboutCard />
+			<SettingItem caption="专业顾问" :value="globalConst.programCounselor" />
+			<AboutCard @click="aboutCardClicked" />
 			<view class="settingsPlaceholder"></view>
 		</scroll-view>
+		<SurpriseCanvas class="surprisePanel" ref="surprisePanel" v-show="surpriseActive" @stoped="surpriseStoped" />
 	</view>
 </template>
 
@@ -31,13 +62,21 @@ const vibrateStateChecked = (checked: boolean) => {
 
 	.settingsScrollView {
 		width: 100%;
-		height: auto;
+		height: 100%;
 
 		// background: var(--color-white);
 		.settingsPlaceholder {
 			width: 100%;
 			height: 70px;
 		}
+	}
+
+	.surprisePanel {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
 	}
 }
 </style>
