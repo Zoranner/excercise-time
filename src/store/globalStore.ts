de4e1d04e1d5e5c866e24178fca8ccbb25c86c36
Zoranner <zoran_young@hotmail.com>
2023-06-28 10:38:04 +0800

    优化封装全局设置的读写逻辑

--- src/store/globalStore.ts
+++ src/store/globalStore.ts
@@ -1,27 +1,58 @@
+import { Preset, PresetsDict } from '@/services/presets/preset'
+
 class GlobalStore {
+	/** 是否第一次打开程序 */
+	firstTime: boolean = true
 	/** 震动状态 */
-	public vibrateState: Ref<boolean> = ref(false)
+	vibrateState: Ref<boolean> = ref(true)
+	/** 预设字典 */
+	presetsDict: PresetsDict = new PresetsDict()
 	/** 选中预设的Index */
-	public presetSelected: Ref<number> = ref(0)
+	presetSelect: Ref<string> = ref('')
 
 	constructor() {
 		this.load()
 	}
 
 	load(): void {
+		this.firstTime = this.toBoolean(Taro.getStorageSync('firstTime'))
 		this.vibrateState.value = this.toBoolean(Taro.getStorageSync('vibrateState'))
-		this.presetSelected.value = this.toNumber(Taro.getStorageSync('presetSelected'))
-		// console.log('vibrateState', this.vibrateState.value)
-		// console.log('presetSelected', this.presetSelected.value)
+		this.presetsDict = this.toPresetsDict(Taro.getStorageSync('presetsDict'))
+		this.presetSelect.value = Taro.getStorageSync('presetSelect')
+
+		if (this.firstTime) {
+			if (this.presetsDict.length() === 0) {
+				for (let i = 1; i <= 9; i++) {
+					this.presetsDict.add(new Preset(`锻炼时间${i}`))
+				}
+				this.presetSelect.value = this.presetsDict.keys()[0]
+				this.firstTime = false
+			}
+		}
+		else {
+			if (!this.presetsDict.contains(this.presetSelect.value)) {
+				this.presetSelect.value = this.presetsDict.keys()[0]
+			}
+		}
 	}
 
 	toBoolean(str: string): boolean {
-		return (/true/i).test(str)
+		return !(/false/i).test(str)
 	}
 
 	toNumber(str: string): number {
 		let result = parseInt(str)
-		return isNaN(result) ? 0 : result
+		return isNaN(result) ? -1 : result
+	}
+
+	toPresetsDict(str: string): PresetsDict {
+		try {
+			let result = JSON.parse(str)
+			return result === null || result === undefined ? new PresetsDict() : Object.assign(new PresetsDict(), result)
+		}
+		catch (e) {
+			return new PresetsDict()
+		}
 	}
 
 	nullOrEmpty(str: string): boolean {
@@ -29,8 +60,10 @@ class GlobalStore {
 	}
 
 	save(): void {
+		Taro.setStorageSync('firstTime', this.firstTime)
 		Taro.setStorageSync('vibrateState', this.vibrateState.value)
-		Taro.setStorageSync('presetSelected', this.presetSelected.value)
+		Taro.setStorageSync('presetsDict', JSON.stringify(this.presetsDict))
+		Taro.setStorageSync('presetSelect', this.presetSelect.value)
 	}
 }
 
