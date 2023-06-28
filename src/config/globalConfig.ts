6057c579f7fa0398521dc015a3a8ebb04411e69a
Zoranner <zoran_young@hotmail.com>
2023-06-29 07:59:36 +0800

    优化全局变量管理类的结构

---
+++ src/config/globalConfig.ts
@@ -0,0 +1,81 @@
+class GlobalConfig {
+	/** 程序版本 */
+	programVersion: string
+	/** 程序设计 */
+	programAuthor: string
+	/** 美术设计 */
+	programDesign: string
+	/** 专业顾问 */
+	programCounselor: string
+	/** 屏幕宽度 */
+	screenWidth: number
+	/** 屏幕高度 */
+	screenHeight: number
+	/** 状态栏高度 */
+	statusBarHeight: number
+	/** 标题栏高度 */
+	titleBarHeight: number
+	/** 应用头部高度 */
+	appHeaderHeight: number
+	/** 标题栏文字大小 */
+	titleBarFontSize: number
+
+
+	ref = reactive({
+		/** 计时器状态 */
+		timerState: 0,
+		/** 标签栏选中的Index */
+		tabBarSelected: 2
+	})
+
+	constructor() {
+		this.getProgramVersion()
+		this.setAboutInfo()
+		this.getSystemInfo()
+		console.log('globalConfig', this)
+	}
+
+	/** 获取程序版本 */
+	private getProgramVersion(): void {
+		let envVersion = Taro.getAccountInfoSync().miniProgram.envVersion
+		let readableEnvVersion = () => {
+			switch (envVersion) {
+				case 'develop':
+					return '开发版'
+				case 'trial':
+					return '体验版'
+				case 'release':
+					return '正式版'
+				default:
+					return '未知'
+			}
+		}
+		let appVersion = Taro.getAccountInfoSync().miniProgram.version
+		this.programVersion = `${readableEnvVersion()} (${appVersion === '' ? '#' : appVersion})`
+	}
+
+	/** 设置关于信息 */
+	private setAboutInfo(): void {
+		this.programAuthor = '杨新冉'
+		/** 美术设计 */
+		this.programDesign = '冯智毅, 杨新冉'
+		/** 专业顾问 */
+		this.programCounselor = '沈钰宁'
+	}
+
+	/** 获取系统信息 */
+	private getSystemInfo(): void {
+		// 获取系统信息
+		let { statusBarHeight, screenWidth, screenHeight } = Taro.getSystemInfoSync()
+		// 获取胶囊信息
+		let { height, top } = Taro.getMenuButtonBoundingClientRect()
+		this.screenWidth = screenWidth
+		this.screenHeight = screenHeight
+		this.statusBarHeight = statusBarHeight ?? 0
+		this.titleBarHeight = height + (top - statusBarHeight!) * 2
+		this.appHeaderHeight = this.statusBarHeight + this.titleBarHeight
+		this.titleBarFontSize = screenHeight * 0.0015
+	}
+}
+
+export const globalConfig = new GlobalConfig()
