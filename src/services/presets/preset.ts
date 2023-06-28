d022c799e98d5281a7bebbdc62621ed9f30fb169
Zoranner <zoran_young@hotmail.com>
2023-06-28 09:17:15 +0800

    新增计时器预设类和其管理类

---
+++ src/services/presets/preset.ts
@@ -0,0 +1,89 @@
+import { nanoid } from 'nanoid/non-secure'
+
+class Preset {
+	/** 唯一标识 */
+	readonly id: string
+	/** 标题 */
+	caption: string
+	/** 准备时间 */
+	prepareTime: number
+	/** 锻炼时间 */
+	exerciseTime: number
+	/** 周期 */
+	cycle: number
+	/** 休息时间 */
+	cycleRestTime: number
+	/** 循环 */
+	loop: number
+	/** 组间休息时间 */
+	loopRestTime: number
+	/** 冷却时间 */
+	coolingTime: number
+
+	constructor(
+		caption: string = '锻炼时间',
+		prepareTime: number = 5,
+		exerciseTime: number = 10,
+		cycle: number = 10,
+		cycleRestTime: number = 5,
+		loop: number = 3,
+		loopRestTime: number = 30,
+		coolingTime: number = 30
+	) {
+		this.id = nanoid(10)
+		this.caption = caption
+		this.prepareTime = prepareTime
+		this.exerciseTime = exerciseTime
+		this.cycle = cycle
+		this.cycleRestTime = cycleRestTime
+		this.loop = loop
+		this.loopRestTime = loopRestTime
+		this.coolingTime = coolingTime
+	}
+}
+
+class PresetsDict {
+	private dict: { [key: string]: Preset } = {}
+
+	constructor() { }
+
+	add(preset: Preset): void {
+		if (this.dict[preset.id] !== undefined) {
+			return
+		}
+		this.dict[preset.id] = preset
+	}
+
+	get(id: string): Preset {
+		return this.dict[id]
+	}
+
+	keys(): string[] {
+		return Object.keys(this.dict)
+	}
+
+	values(): Preset[] {
+		return Object.values(this.dict)
+	}
+
+	contains(id: string): boolean {
+		return this.dict[id] !== undefined
+	}
+
+	length(): number {
+		return Object.keys(this.dict).length
+	}
+
+	remove(id: string): void {
+		if (this.dict[id] === undefined) {
+			return
+		}
+		delete this.dict[id]
+	}
+
+	clear(): void {
+		this.dict = {}
+	}
+}
+
+export { Preset, PresetsDict }
