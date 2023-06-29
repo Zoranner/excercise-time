eadd7e88aa427af4c4703ee8e54383fa5b6b7690
Zoranner <zoran_young@hotmail.com>
2023-06-29 08:08:12 +0800

    新增全局可使用的类型转换工具类

---
+++ src/utils/typeConvert.ts
@@ -0,0 +1,22 @@
+class TypeConvert {
+    toBoolean(str: string): boolean {
+        return !(/false/i).test(str)
+    }
+
+    toNumber(str: string): number {
+        let result = parseInt(str)
+        return isNaN(result) ? -1 : result
+    }
+
+    toHumanTime(seconds: number): string {
+        var minutes = Math.min(Math.round(seconds / 60) % 60, 99);
+        var seconds = seconds % 60;
+        return `${this.prefixInteger(minutes, 2)}:${this.prefixInteger(seconds, 2)}`
+    }
+
+    private prefixInteger(num: number, length: number) {
+        return ("0" + num).slice(-length);
+    }
+}
+
+export const typeConvert = new TypeConvert()
