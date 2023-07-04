3c357a37619acb80e041572e1eabc39518c72bec
Zoranner <zoran_young@hotmail.com>
2023-07-04 15:19:11 +0800

    优化输入框和输入项的交互逻辑

--- src/components/global/editor-layer/input-item.vue
+++ src/components/global/editor-layer/input-item.vue
@@ -1,4 +1,8 @@
 <script lang="ts" setup>
+import '@/utils/extensions/json.extension'
+
+const emits = defineEmits(['update'])
+
 const props = defineProps({
     caption: {
         type: String,
@@ -7,16 +11,50 @@ const props = defineProps({
     value: {
         type: String,
         default: '锻炼时间'
+    },
+    maxLength: {
+        type: Number,
+        default: 6
     }
 })
+
+const inputBoxRef = ref()
+const inputFocusRef = ref(false)
+const inputValueRef = ref(props.value)
+const inputSelectionStartRef = ref(0)
+const inputSelectionEndRef = ref(inputValueRef.value.length)
+
+const inputItemClicked = () => {
+    Vibrate.short('light')
+    inputSelectionEndRef.value = inputValueRef.value.length
+    inputFocusRef.value = true
+}
+
+const inputBoxBlur = (e: any) => {
+    inputValueRef.value = e.target.value
+    if (inputValueRef.value.length > props.maxLength) {
+        inputValueRef.value = inputValueRef.value.slice(0, props.maxLength)
+    }
+    inputFocusRef.value = false
+    emits('update', inputValueRef.value)
+}
+
+watch(
+    () => props.value,
+    (value) => {
+        inputValueRef.value = value
+    }
+)
 </script>
 
 <template>
-    <view class="inputItemPanel w-100% flex items-center justify-center">
+    <view class="inputItemPanel w-100% flex items-center justify-center" @click="inputItemClicked">
         <view class="inputItemCaption">
             {{ props.caption }}
         </view>
-        <input class="inputItemValue flex-1" type="text" placeholder="锻炼时间" :value="props.value" maxlength="16" />
+        <input class="inputItemValue flex-1" ref="inputBoxRef" type="text" placeholder="锻炼时间" :focus="inputFocusRef"
+            :value="inputValueRef" :selectionStart="inputSelectionStartRef" :selectionEnd="inputSelectionEndRef"
+            @blur="inputBoxBlur" />
     </view>
 </template>
 
@@ -30,7 +68,7 @@ const props = defineProps({
     background: var(--color-ts-dark-gray);
 
     .inputItemCaption {
-        width: 200px;
+        width: 220px;
         font-size: 1.35rem;
         margin-left: 50px;
         filter: var(--shadow-drop-white);
